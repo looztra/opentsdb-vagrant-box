f3d955b3a2fe13a6e29f8152a37b305823f6e4d0
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2012-11-05 19:42:22 +0100

    introduce dep to yum recipe, specify supported os in opentsdb recipe

--- chef/cookbooks/opentsdb/metadata.rb
+++ chef/cookbooks/opentsdb/metadata.rb
@@ -4,3 +4,9 @@ license          "All rights reserved"
 description      "Installs/Configures opentsdb"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+depends "yum"
+recipe "opentsdb", "Installs HBase and OpenTSDB from source"
+%w{ centos redhat scientific fedora }.each do |os|
+  supports os
+end
+
